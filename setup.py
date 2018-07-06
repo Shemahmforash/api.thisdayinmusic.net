9e33c570eef3c3cd9ea7c2370a3a2e4878c8b74d
Carlos Rosão <shemahmforash@gmail.com>
2018-07-06 18:31:03 +0100

    Cleanup dependencies

--- setup.py
+++ setup.py
@@ -7,17 +7,7 @@ setup(
     name='thisdayinmusic',
     version=__version__,
     packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'flask',
-        'flask-sqlalchemy',
-        'flask-restful',
-        'flask-migrate',
-        'flask-jwt-extended',
-        'flask-marshmallow',
-        'marshmallow-sqlalchemy',
-        'python-dotenv',
-        'passlib'
-    ],
+    install_requires=[],
     entry_points={
         'console_scripts': [
             'thisdayinmusic = thisdayinmusic.manage:cli'
--- thisdayinmusic/manage.py
+++ thisdayinmusic/manage.py
@@ -17,7 +17,6 @@ def cli():
 @cli.command("init")
 def init():
     """Init application, create database tables
-    and create a new user named admin with password admin
     """
     from thisdayinmusic.extensions import db
     from thisdayinmusic.models import User
